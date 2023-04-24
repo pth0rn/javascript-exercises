68109c21ccc0a20114f842421f19ac6cc5c2a660
p <p@zor.(none)>
2023-04-24 13:50:05 -0400

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let re = /[a-zA-Z]/;
+    word = word.split('')
+        .filter(letter => re.test(letter))
+        .join('')
+        .toLowerCase()
 
+    if (word === word.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// 'A car, a man, a maraca.'.split('').filter(letter => re.test(letter)).join('').toLowerCase();
