086e84839e16d3b858bf84c500a138ec804fce50
p <p@zor.(none)>
2023-04-24 07:36:00 -0400

    complete 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === 0) return '';
+    if (num<1) return 'ERROR';
+    let fullStr = '';
+    for (let i = 1; i <= num; i++) {
+        fullStr += str;
+    }
+    return fullStr;
 };
 
 // Do not edit below this line
