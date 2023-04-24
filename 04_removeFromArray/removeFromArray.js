3eb9ce669006b13be6b0aff7739e4805f2754a13
p <p@zor.(none)>
2023-04-24 08:17:42 -0400

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    for (el of remove) {
+        if (arr.includes(el)) {
+            arr.splice(arr.indexOf(el),1)
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
