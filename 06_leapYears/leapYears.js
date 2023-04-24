f30da637f6e6b02c376a53d025d850f6d41ac58c
p <p@zor.(none)>
2023-04-24 10:56:07 -0400

    refactor 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,5 @@
 const leapYears = function (year) {
-    if (year % 4 === 0) {
-        if (year % 100 === 0 && year % 400 !== 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-    return false;
-
+    return (year % 4 ===0) && (year % 100 !== 0 || year % 400 === 0)
 }
 
 
