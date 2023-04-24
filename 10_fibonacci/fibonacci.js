806ede00bfa0a0c394e3a393f95ac42b7c43cff7
p <p@zor.(none)>
2023-04-24 14:52:09 -0400

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num<0) return "OOPS";
+    let previous = 0;
+    let current = 1; //starting number
+    let temp = 0
+    for (let i = 1; i < num; i++) {
+        temp = current + previous;
+        previous = current;
+        current = temp;
+    }
+    return current;
 };
 
 // Do not edit below this line
