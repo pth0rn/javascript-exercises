d27ca7dab009aa381cf2ecabcc8d50d4c73f3d33
p <p@zor.(none)>
2023-04-24 12:54:59 -0400

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(...nums) {
+  return nums.reduce( (accum, num) => accum+num )
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+	return nums.reduce( (accum, num) => accum-num )
 };
 
-const sum = function() {
-	
+const sum = function(numArr) { 
+  if (numArr.length === 0) return 0;
+	return numArr.reduce( (accum, num) => accum+num )
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce( (accum, num) => accum*num )
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num**power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for (let i = 1; i<=num; i++){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
