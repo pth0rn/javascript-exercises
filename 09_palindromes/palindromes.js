a164d5e8c64cd5eb4d7fbe176cb02cb5c713e8a2
p <p@zor.(none)>
2023-04-24 14:02:29 -0400

    update 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,7 @@
 const palindromes = function (word) {
-    let re = /[a-zA-Z]/;
-    word = word.split('')
-        .filter(letter => re.test(letter))
-        .join('')
-        .toLowerCase()
-
-    if (word === word.split('').reverse().join('')) {
-        return true;
-    } else {
-        return false;
-    }
+    const newWord = word.toLowerCase().replace(/[^a-z]/g, '')
+    return newWord === newWord.split('').reverse().join('')
 };
 
 // Do not edit below this line
 module.exports = palindromes;
-
-// 'A car, a man, a maraca.'.split('').filter(letter => re.test(letter)).join('').toLowerCase();
