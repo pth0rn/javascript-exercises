299e6bf8a4048f7fd35023c99c74a686373771fb
p <p@zor.(none)>
2023-04-25 08:41:20 -0400

    tried sort method instead of reduce for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,9 @@
 const findTheOldest = function (people) {
-    return people.reduce((oldestPerson, currentPerson) => {
-  
-      var oldestPersonsAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth || (new Date()).getFullYear() - oldestPerson.yearOfBirth
-      var currentPersonsAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth || (new Date()).getFullYear() - currentPerson.yearOfBirth;
-          
-      if (currentPersonsAge > oldestPersonsAge) {
-          return currentPerson
-      } else {
-          return oldestPerson
-      }
-    })
-  };
+    return people.sort( (a,b) => {
+        return ((b.yearOfDeath || (new Date()).getFullYear() ) - b.yearOfBirth) - 
+                   ((a.yearOfDeath || (new Date()).getFullYear()) - a.yearOfBirth) 
+    })[0]
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
