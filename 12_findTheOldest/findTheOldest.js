f9d4c32a271dba654bd46dbd21dc8b1609860885
p <p@zor.(none)>
2023-04-25 08:19:03 -0400

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+  
+      var oldestPersonsAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth || (new Date()).getFullYear() - oldestPerson.yearOfBirth
+      var currentPersonsAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth || (new Date()).getFullYear() - currentPerson.yearOfBirth;
+          
+      if (currentPersonsAge > oldestPersonsAge) {
+          return currentPerson
+      } else {
+          return oldestPerson
+      }
+    })
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
